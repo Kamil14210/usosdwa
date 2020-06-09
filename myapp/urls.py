e51cdcd86de3a880a13326e91716d113e3ed8685
Bartlomiej Jakubowski <enshamrat@gmail.com>
2020-06-09 20:28:33 +0200

    urls update

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='main-menu'),  ##edupa cycki zrob tu logowanie i guziczki
 	path('lista_studentow',views.students_index, name='students-index'),
-	#path('usosdwa/<int:book_id>', views.book_detail, name='books-details'),
 	path('add', views.student_add, name='student-add')
 ]
